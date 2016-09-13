8adefcd03fd6bb5570809d8506c638ad7b4ff5b2
Alex Erokhin <alex@mspektr.ru>
2016-09-13 11:51:22 +0300

    Правки зон/серверов pro

--- src/whois.pro.php
+++ src/whois.pro.php
@@ -6,16 +6,16 @@
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- * 
+ *
  * @link http://phpwhois.pw
  * @copyright Copyright (C)1999,2005 easyDNS Technologies Inc. & Mark Jeftovic
  * @copyright Maintained by David Saez
@@ -30,8 +30,22 @@ if (!defined('__PRO_HANDLER__'))
 class pro_handler {
 
     function parse($data, $query) {
+        $items = array(
+            'Domain Name:' => 'domain.name',
+            'Domain Status:' => 'domain.status',
+            'Name Server:' => 'domain.nserver.',
+            'Creation Date:' => 'domain.created',
+            'Registry Expiry Date:' => 'domain.expires',
+            'Registrant Organization:' => 'owner.type',
+            'Registrant Name:' => 'owner.organization',
+            'Registrant Phone:' => 'owner.phone',
+            'Registrant Fax:' => 'owner.fax',
+            'Registrant Email:' => 'owner.email'
+        );
         $r = array();
-        $r['regrinfo'] = generic_parser_b($data['rawdata']);
+        $r['regrinfo'] = generic_parser_b($data['rawdata'], $items, 'dmy');
+        if (empty($r['regrinfo']['domain']['status']))
+            $r['regrinfo']['registered'] = 'no';
         $r['regyinfo']['referrer'] = 'http://www.registrypro.pro';
         $r['regyinfo']['registrar'] = 'RegistryPRO';
         return $r;
